84166b3f750316d79ee1957cdf01397edabe5801
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2023-08-21 10:17:19 +0200

    Test interactivity

--- frontend/tests/InfoItem.test.tsx
+++ frontend/tests/InfoItem.test.tsx
@@ -1,3 +1,5 @@
+import userEvent from '@testing-library/user-event';
+
 import '@testing-library/jest-dom';
 
 import InfoItem from '../src/components/InfoItem';
@@ -13,8 +15,33 @@ describe('InfoItem', () => {
 
         render(<InfoItem info={info} />);
 
-        const orcidElem = document.querySelector('span.info-item') as HTMLSpanElement;
+        const infoItem = document.querySelector('span.info-item') as HTMLSpanElement;
 
-        expect(orcidElem).toHaveTextContent(`${info.foo} - ${info.bar}`);
+        expect(infoItem).toHaveClass('info-item');
+        expect(infoItem).not.toHaveClass('active');
+        expect(infoItem).toHaveTextContent(`${info.foo} - ${info.bar}`);
+    });
+
+    it('marks as active', async () => {
+        const info: Info = {
+            foo: 'foo',
+            bar: 42,
+        };
+
+        render(<InfoItem info={info} />);
+
+        const infoItem = document.querySelector('span.info-item') as HTMLSpanElement;
+
+        await userEvent.click(infoItem);
+
+        expect(infoItem).toHaveClass('info-item');
+        expect(infoItem).toHaveClass('active');  // InfoItem is now active
+        expect(infoItem).toHaveTextContent(`${info.foo} - ${info.bar}`);
+
+        await userEvent.click(infoItem);
+
+        expect(infoItem).toHaveClass('info-item');
+        expect(infoItem).not.toHaveClass('active');  // InfoItem is inactive again
+        expect(infoItem).toHaveTextContent(`${info.foo} - ${info.bar}`);
     });
 });
